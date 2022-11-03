3ef5f76aef245e66aca5ef9b7ec948f62769081f
TechWiz-3 <programmingplus1@gmail.com>
2022-11-03 23:36:41 +1100

    📦 NEW: json format fully working

--- kadoo_utils.py
+++ kadoo_utils.py
@@ -17,15 +17,20 @@ class Quadrant:
             return "[b][white]Less Important", "", txt
 
     @classmethod
-    def get_all_quadrants(self, json):
+    def get_all_quadrants(self, j_data):
         quadrants = []
         top_row = []
         bottom_row = []
-        for i in range(1,5):
+        for i in range(1, 5):
+            contents_str = ""
             if i == 1 or i == 2:
-                top_row.append(json[str(i)])
+                for item in list(j_data[str(i)].values())[0]:
+                    contents_str += item
+                top_row.append(contents_str)
             else:
-                bottom_row.append(json[str(i)])
+                for item in list(j_data[str(i)].values())[0]:
+                    contents_str += item
+                bottom_row.append(contents_str)
         quadrants.append(top_row)
         quadrants.append(bottom_row)
         return quadrants
@@ -44,23 +49,39 @@ class Quadrant:
         quadrant = str(quadrant)
         with open("table.json", "r+") as file:
             table = json.load(file)
-            if table[quadrant] == "":
-                table[quadrant] += info
+            quadrant_content = list(table[quadrant].values())[0]
+            if not len(quadrant_content):
+                quadrant_content.append(f"\n{info}")
             else:
-                table[quadrant] += f"\n{info}"
+                quadrant_content.append(info)
+            table[quadrant] = {"content": quadrant_content}
             file.seek(0)
             json.dump(table, file, indent=4)
-        # write to quadrant
-        return None
 
 
     @classmethod
     def remove_entry(self, quadrant, name):
-        import re
         quadrant = str(quadrant)
         with open("table.json", "r+") as file:
             table = json.load(file)
-            table[quadrant] = re.sub(rf"\n{name}", '', table[quadrant])
+            quadrant_content = list(table[quadrant].values())[0]
+            for i, item in enumerate(quadrant_content):
+                if name == item or f"\n{name}" == item:
+                    if i == 0:
+                        quadrant_content.remove(name)
+                        # remove the newline on next entry
+                        quadrant_content = Quadrant.update_newlines(quadrant_content)
+                    else:
+                        quadrant_content.remove(f"\n{name}")
+            table[quadrant] = {"content": quadrant_content}
         with open("table.json", "w") as file:
             json.dump(table, file, indent=4)
 
+
+    @classmethod
+    def update_newlines(self, content) -> str:
+        """remove the newline char on the next line after deleting the first
+        entry"""
+        import re
+        content[0] = re.sub("\n", "",  content[0])
+        return content
