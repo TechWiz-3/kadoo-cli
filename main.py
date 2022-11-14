474b43cfd0e4287be3cb86face43f49d2b637fe0
TechWiz-3 <programmingplus1@gmail.com>
2022-11-14 13:49:23 +1100

    📦 NEW: subparsers and new tables

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sys import argv
 import sys
 
 from kadoo_utils import Quadrant
+from multiple_table_management_utils import get_yaml_config
+from multiple_table_management_utils import get_tables_names
 
 def base_table(new=None,info=None, quadrant=0):
     table = Table(box=box.MINIMAL)
@@ -53,17 +55,46 @@ def crazy_table(rows, default=True):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="command")
+    tables_used = get_tables_names()
+    subparsers_loaded = []
+    for table in tables_used:
+        print(table)
+        subparsers.add_parser(name=table[0], help="ayoo")
+        subparsers_loaded.append((table[0], table[1]))
+
     parser.add_argument("-a", "--add", type=str, required=argv in ("-a", "--add", "-r", "--remove"))
     parser.add_argument("-q", "--quadrant", type=int, choices={1, 2, 3, 4}, required="-a" in argv or "--add" in argv)
     parser.add_argument("-b", "--base", action="store_true", required=False)
     parser.add_argument("-r", "--remove", type=str, required=False)
+    parser.add_argument("-ct", "--create-table", action="store_true", required=argv in ("-n", "--name"))
+    parser.add_argument("-n", "--name", type=str, required=False)
     parser.add_argument("--green", action="store_true", required=False)
     parser.add_argument("--purple", action="store_true", required=False)
-    parser.add_argument("--cat", action="store_true", required=False)
+    parser.add_argument("--cap", action="store_true", required=False)
     parser.add_argument("--solarized", action="store_true", required=False)
     parser.add_argument("--nord", action="store_true", required=False)
     parser.add_argument("--nord-aurora", action="store_true", required=False)
     args = parser.parse_args()
+
+    if args.create_table:
+        name = args.name
+        # change this to absolute path later
+        location = f"./{args.name.lower()}"
+        from multiple_table_management_utils import create_new_table
+        create_new_table(name, location)
+    else:  # load subparsers used, if any
+        for table_subparser in subparsers_loaded:
+            if args.command == table_subparser[0]:
+                selected_table_path = table_subparser[1]
+                # don't want to be working with
+                # multiple tables at the same time
+                break
+        else:
+            from multiple_table_management_utils import get_def_table
+            # use default
+            selected_table_path = get_def_table()
 
 
     if args.base:
@@ -84,7 +115,7 @@ if __name__ == "__main__":
 
 
     import json
-    with open("table.json") as j_file:
+    with open(selected_table_path) as j_file:
         j = json.load(j_file)
     rows = Quadrant.get_all_quadrants(j)
     if args.green:
---
+++ multiple_table_management_utils.py
@@ -0,0 +1,44 @@
+import yaml
+
+
+def get_yaml_config(yaml_path="tables.yaml"):
+    with open(yaml_path) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        return data
+
+def get_tables_names() -> list:
+    """[(table_name, table_path)]"""
+    tables = []
+    data = get_yaml_config()
+    for table_info in data["tables"].items():
+        table_data = (table_info[0], table_info[1]["Location"])
+        tables.append(table_data)
+    return tables
+
+
+def get_def_table() -> list:
+    """gets default table"""
+    data = get_yaml_config()
+    return data["default_table_path"]
+
+
+def create_new_table(name, location):
+        config = get_yaml_config()
+        config["tables"][name] = ({'Location': location})
+        print(config)
+        with open("test_tables.yaml", "w") as f:
+            yaml.dump(config, f)
+
+def delete_table(name):
+    config = get_yaml_config()
+    try:
+        config["tables"].pop(name)
+    except KeyError:
+        print(f"Table name not found for deletion '{name}'")
+
+
+if __name__ == "__main__":
+    #print(get_yaml_config())
+    #print(get_tables_names())
+   # print(get_def_table())
+    print(delete_table(name="ayo"))
